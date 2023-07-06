60fa94dbda8adfac136bdeae7795eb4115929601
santiarroyave <santi98cv@hotmail.com>
2023-07-07 01:36:37 +0200

    Ej1. limpiando codigo

--- problema01/src/app/app.component.ts
+++ problema01/src/app/app.component.ts
@@ -19,32 +19,17 @@ export class AppComponent {
     grupo:new FormControl('')
   });
 
-
-  datosAgregar = new Array;
-
-  // 1.1 PASANDO VALORES A UN COMPONENTE HIJO
-  variable1 = "¡Hola mundo!";
-  arrayPrueba = ["valor 1", "valor 2", "valor 3", "valor 4"];
-
-  // listaDatos:[string,string,string,string] = ["","","",""];
   listaDatos = new Array();
-
-  prueba1:string = "";
-  prueba2:string = "";
 
   // CONSTRUCTORES
   constructor(){}
 
   // METODOS
   onSubmit(){
-    // console.log(this.formularioUsuario.value["nombre"]);
-    
     // this.listaDatos[0] = this.formularioUsuario.value["nombre"];
     // this.listaDatos[1] = this.formularioUsuario.value["cif"];
     // this.listaDatos[2] = this.formularioUsuario.value["direccion"];
     // this.listaDatos[3] = this.formularioUsuario.value["grupo"];
     this.listaDatos.push(this.formularioUsuario.value);
-
-    // console.log(this.listaDatos);
   }
 }
--- problema01/src/app/listado-clientes/listado-clientes.component.ts
+++ problema01/src/app/listado-clientes/listado-clientes.component.ts
@@ -8,16 +8,5 @@ import { Component, Input } from '@angular/core';
 export class ListadoClientesComponent {
 
   @Input() formularioEnviado: any;
-  // @Input() valor: any;
 
-  ngOnInit(){
-    console.log(this.formularioEnviado);
-    // console.log("Esto es una prueba");
-    
-    // console.log(this.formularioEnviado);
-    // console.log("Fin prueba");
-    
-    // console.log(this.formularioEnviado.value["nombre"]);
-    // console.log("Fin prueba2");
-  }
 }
