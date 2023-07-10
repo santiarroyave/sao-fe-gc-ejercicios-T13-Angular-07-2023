a6f96a70a98e4debf2f702b88e7e731d2a2bcd69
santiarroyave <santi98cv@hotmail.com>
2023-07-10 03:05:36 +0200

    Ejercicio 3

--- README.md
+++ README.md
@@ -31,4 +31,4 @@
 ![imagen](https://github.com/santiarroyave/sao-fe-gc-ejercicios-T13-Angular-07-2023/assets/135848692/faca97c1-f7a6-4cc9-a2c1-b3d9afcd61b9)
 
 ### Ejercicio 3
-
+![imagen](https://github.com/santiarroyave/sao-fe-gc-ejercicios-T13-Angular-07-2023/assets/135848692/69fc8124-1ab3-46af-bf8d-b754cbbd91be)
--- problema03/src/app/tabla/tabla.component.ts
+++ problema03/src/app/tabla/tabla.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-tabla',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabla.component.css']
 })
 export class TablaComponent {
+  @Input() datosTabla:any;
+  @Output() seleccion = new EventEmitter();
 
+  borrar(articulo:any){
+    console.log(`Eliminado: ${articulo.codigo} ${articulo.descripcion} ${articulo.precio}`);
+    // 1. Encontrar su posición dentro de la lista
+    let posicion = this.datosTabla.indexOf(articulo);
+    // 2. Borrar elemento
+    this.datosTabla.splice(posicion, 1);
+  };
+  
+  
+  seleccionar(articulo:any){
+    // Envia los datos del articulo al elemento padre
+    this.seleccion.emit(articulo);
+  };
 }
