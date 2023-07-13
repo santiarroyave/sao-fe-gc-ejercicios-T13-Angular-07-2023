cca8a192616cab2ba4bfe590e19dd8f0b9f8fc5e
santiarroyave <santi98cv@hotmail.com>
2023-07-14 01:07:30 +0200

    Agregada funcionalida de completar tareas

--- problema04/src/app/lista-tareas/lista-tareas.component.ts
+++ problema04/src/app/lista-tareas/lista-tareas.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-lista-tareas',
@@ -10,8 +11,18 @@ export class ListaTareasComponent {
   // Obtenemos el array de tareas del componente padre
   @Input() tareas:any;
 
-  completar(){
-    alert("Se ha completado");
+  completar(tarea:any){
+    // - Importamos el NgClass para poder agregar la clase en el html si la tarea está completada
+    // 1. Encontrar su posición dentro de la lista
+    let posicion = this.tareas.indexOf(tarea);
+    // 2. Alterna entre valor completado o no
+    if(tarea.completado == true){
+      tarea.completado = false;
+    }else{
+      tarea.completado = true;
+    }
+    // 3. Actualiza el array
+    this.tareas.splice(posicion,1,tarea);
   };
   
   eliminar(tarea:any){
@@ -21,3 +32,7 @@ export class ListaTareasComponent {
     this.tareas.splice(posicion, 1);
   };
 }
+
+// Dudas
+// - ¿Que otros métodos se pueden usar en la función completar() que no implique actualizar el objeto completo?
+// - ¿Deberia usar ID en cada tarea para evitar pasar el objeto como argumento o hay más métodos?
