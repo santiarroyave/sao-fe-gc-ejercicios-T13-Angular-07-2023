5e6f1fc469230ebcaa7828db705ae14fa53fc003
santiarroyave <santi98cv@hotmail.com>
2023-07-14 01:36:11 +0200

    Terminado componente del formulario con su variable Output

--- problema04/src/app/formulario-tarea/formulario-tarea.component.ts
+++ problema04/src/app/formulario-tarea/formulario-tarea.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-formulario-tarea',
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class FormularioTareaComponent {
 
+  // Variable para enviar datos al componente padre
+  @Output() tareaNueva:any = new EventEmitter();
+
+  // Atributos
+  titulo = "";
+  descripcion = "";
+  defCompletado = false;
+
+  agregarTarea(){
+    if(this.titulo == ""){
+      alert("Debes introducir al menos un título");
+    }else{
+      // Agregar datos al @Output
+      this.tareaNueva.emit({
+        titulo:this.titulo,
+        descripcion:this.descripcion,
+        completado:this.defCompletado
+      });
+
+      // Resetear datos de los inputs
+      this.titulo = "";
+      this.descripcion = "";
+    }
+  }
 }
+
+// Recordatorios
+// - Importar clases necesarias "Output", "EventEmitter"
+// - Usar el metodo "emit()" para agregar datos al @Output
